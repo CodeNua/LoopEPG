3164e3bb0cb034da12cbebe59a3d514fa1b29292
CodeNua <81170604+CodeNua@users.noreply.github.com>
2021-05-01 18:31:50 +0100

    Add files via upload

--- LoopEPG.py
+++ LoopEPG.py
@@ -2,10 +2,25 @@
 """
 Created on Thu Mar 18 12:19:43 2021
 
-@author: LoopEPG
-Copyright (c) 2021 codeNua
-License: GPLv3, See License File or online
+@author: codenua
+
+LoopEPG
+Copyright (C) 2021  codeNua
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
 import csv, os
 import datetime
 from lxml import etree
